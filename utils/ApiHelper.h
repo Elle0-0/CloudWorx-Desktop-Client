e6c9bd5c6d760dc42e5f464210c0edb51df0272a
Elle0-0 <andreamichellevaz@gmail.com>
2025-06-04 19:14:46 +0100

    api helper

--- utils/ApiHelper.h
+++ utils/ApiHelper.h
@@ -2,6 +2,10 @@
 #define APIHELPER_H
 
 #include <string>
+#include <QCoreApplication>
+#include <QFile>
+#include <QDebug>
+#include <curl/curl.h>
 
 inline size_t writeCallback(void* contents, size_t size, size_t nmemb, void* userp)
 {
@@ -9,6 +13,23 @@ inline size_t writeCallback(void* contents, size_t size, size_t nmemb, void* use
     std::string* str = static_cast<std::string*>(userp);
     str->append(static_cast<char*>(contents), totalSize);
     return totalSize;
+}
+
+inline bool setCurlCACert(CURL* curl)
+{
+    QString certPath = QCoreApplication::applicationDirPath() + "/certs/cacert.pem";
+    if (!QFile::exists(certPath)) {
+        qWarning() << "[setCurlCACert] CA cert file not found at:" << certPath;
+        return false;
+    }
+
+    CURLcode result = curl_easy_setopt(curl, CURLOPT_CAINFO, certPath.toStdString().c_str());
+    if (result != CURLE_OK) {
+        qWarning() << "[setCurlCACert] Failed to set CAINFO:" << curl_easy_strerror(result);
+        return false;
+    }
+
+    return true;
 }
 
 #endif // APIHELPER_H
